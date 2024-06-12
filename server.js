7c2d51fba0ff924a3215e80c8ae0992e72ade3cf
adomasr <adomas.razmus@gmail.com>
2024-06-12 23:04:20 +0100

    permutation logic

--- server.js
+++ server.js
@@ -7,15 +7,82 @@ const port = 5000;
 server.use(cors());
 server.use(express.json());
 
+
+function getPermutations(input) {
+    let permutations = [''];
+    
+    // Iterate over each character in the input string
+    for (let char of input) {
+        let newPermutations = [];
+        // Iterate over existing permutations and insert current character at every position
+        for (let permutation of permutations) {
+            for (let i = 0; i <= permutation.length; i++) {
+                let newPermutation = permutation.slice(0, i) + char + permutation.slice(i);
+                newPermutations.push(newPermutation);
+            }
+        }
+        permutations = newPermutations;
+    }
+    
+    return permutations;
+};
+
+// const getWordsArray = (reqContent) {
+
+// };
+
+
+// console.log(getPermutations('car'));
+
 server.post('/test', (req, res)=> {
-    console.log(req.body.text);
-    res.status(200);        /*chain the status and send */
-    res.send({
+
+    let outputString = '';
+
+    const wordsArray = req.body.text.split('\n')
+        .filter((item)=> item !== '');
+
+        wordsArray.forEach((word=> {
+
+            let stringWithPermutaions = '';
+
+            const permutationsArray = getPermutations(word);
+
+            const wordPermutationArray = permutationsArray.filter((permutation)=> permutation === word);
+
+
+            wordPermutationArray.forEach((wordPermutation)=> {
+                stringWithPermutaions = stringWithPermutaions + word + ' ' + wordPermutation;
+            });
+
+            outputString = outputString + ' ' + stringWithPermutaions; 
+        }));
+
+        console.log(outputString);
+
+    // console.log(getPermutations(wordsArray[0]));
+
+    res.status(200).send({
         "message": "the server responded!!!"
     });
 });
 
+
+
 server.listen(port, ()=> console.log(`the server is on ${port}`));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
